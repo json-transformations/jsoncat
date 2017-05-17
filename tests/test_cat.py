fbbc7a716c452cf793499a93babedb6096f58ebf
bpeterso2000 <bpeterso2000@yahoo.com>
2017-05-16 21:53:25 -0400

    Change import method for Travis CI

--- tests/test_cat.py
+++ tests/test_cat.py
@@ -5,9 +5,7 @@ import click.testing
 from click import Command, Context
 from mock import MagicMock
 
-from jsoncat.cli import main
-from jsoncat.core import load_json_files
-
+import jsoncat
 
 D1 = {"asteroid": "433 Eros"}
 D2 = {"asteroid": "951 Gaspra"}
@@ -22,13 +20,13 @@ def runner():
 
 
 def test_no_args_with_stdin(runner):
-    result = runner.invoke(main, args=[], input=J1)
+    result = runner.invoke(jsoncat.cli.main, args=[], input=J1)
     assert result.exit_code == 0
     assert eval(result.output) == D1
 
 
 def test_stdin_arg(runner):
-    result = runner.invoke(main, args=['-'], input=J1)
+    result = runner.invoke(jsoncat.cli.main, args=['-'], input=J1)
     assert result.exit_code == 0
     assert eval(result.output) == D1
 
@@ -37,7 +35,7 @@ def test_single_filename(runner):
     with runner.isolated_filesystem():
         with open('t1.json', 'w') as f:
             f.write(J1)
-        result = runner.invoke(main, args=['t1.json'])
+        result = runner.invoke(jsoncat.cli.main, args=['t1.json'])
         assert result.exit_code == 0
         assert eval(result.output) == D1
 
@@ -46,7 +44,8 @@ def test_stdin_arg_plus_filename(runner):
     with runner.isolated_filesystem():
         with open('t2.json', 'w') as f:
             f.write(J2)
-        result = runner.invoke(main, args=['-', 't2.json'], input=J1)
+        result = runner.invoke(jsoncat.cli.main, args=['-', 't2.json'],
+                               input=J1)
         assert result.exit_code == 0
         assert eval(result.output) == [D1, D2]
 
@@ -55,7 +54,8 @@ def test_stdin_as_second_arg(runner):
     with runner.isolated_filesystem():
         with open('t1.json', 'w') as f:
             f.write(J1)
-        result = runner.invoke(main, args=['t1.json', '-'], input=J2)
+        result = runner.invoke(jsoncat.cli.main, args=['t1.json', '-'],
+                               input=J2)
         assert result.exit_code == 0
         assert eval(result.output) == [D1, D2]
 
@@ -66,7 +66,7 @@ def test_two_filenames(runner):
             f.write(J1)
         with open('t2.json', 'w') as f:
             f.write(J2)
-        result = runner.invoke(main, args=['t1.json', 't2.json'])
+        result = runner.invoke(jsoncat.cli.main, args=['t1.json', 't2.json'])
         assert result.exit_code == 0
         assert eval(result.output) == [D1, D2]
 
@@ -77,30 +77,30 @@ def test_two_filenames_reversed(runner):
             f.write(J1)
         with open('t2.json', 'w') as f:
             f.write(J2)
-        result = runner.invoke(main, args=['t2.json', 't1.json'])
+        result = runner.invoke(jsoncat.cli.main, args=['t2.json', 't1.json'])
         assert result.exit_code == 0
         assert eval(result.output) == [D2, D1]
 
 
 def test_indented_json(runner):
-    result = runner.invoke(main, args=[], input=J1)
+    result = runner.invoke(jsoncat.cli.main, args=[], input=J1)
     assert result.exit_code == 0
     assert len(result.output.splitlines()) == 3
 
 
 def test_compact_json(runner):
-    result = runner.invoke(main, args=['--compact'], input=J1)
+    result = runner.invoke(jsoncat.cli.main, args=['--compact'], input=J1)
     assert result.exit_code == 0
     assert len(result.output.splitlines()) == 1
 
 
 def test_json_decode_error(runner):
-    result = runner.invoke(main, args=[], input=JE)
+    result = runner.invoke(jsoncat.cli.main, args=[], input=JE)
     assert result.exit_code == 1
 
 
 def test_no_args_without_stdin(runner):
-    result = runner.invoke(main, args=[])
+    result = runner.invoke(jsoncat.cli.main, args=[])
     assert result.exit_code == 1
 
 
@@ -108,4 +108,4 @@ def test_no_args_with_no_stdin_and_is_tty():
     ctx = Context(Command('jsncat'))
     ctx.get_usage = MagicMock(return_value='Usage: jsncat ...')
     with pytest.raises(SystemExit):
-        load_json_files(ctx=ctx, is_tty=True)
+        jsoncat.core.load_json_files(ctx=ctx, is_tty=True)
