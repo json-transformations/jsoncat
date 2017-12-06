97255aae17b99f4ef2eefafbb380dc1f19f8e1cf
Brian Peterson <bpeterso2000@yahoo.com>
2017-12-05 22:26:12 -0500

    Update installation requirements

--- setup.py
+++ setup.py
@@ -1,28 +1,47 @@
-import re
 import ast
-from setuptools import setup
+import re
+from setuptools import setup, find_packages
 
-requirements = ['click>=6.7']
-test_requirements = ['flake8', 'pytest-cov']
-
+# retrieve __version__ number from __init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
-
-with open('jsoncat/__init__.py', 'rb') as f:
+with open('cli.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
+# retrieve long description from README.rst
+with open('README.rst', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
 setup(
-    name='json-cat',
+    name="jsoncat",
     version=version,
-    author='Brian Peterson',
-    author_email='bpeterso2000@yahoo.com',
-    url='http://github.com/json-transformations/jsoncat',
-    packages=['jsoncat'],
-    description='Concatenate JSON FILE(s), or STDIN, format to STDOUT.',
-    classifiers=['License :: OSI Approved :: MIT License',
-                 'Programming Language :: Python',
-                 'Programming Language :: Python :: 3'],
-    install_requires=requirements,
-    tests_require=test_requirements,
-    entry_points={'console_scripts': ['jsncat=jsoncat.cli:main']}
+    url="http://github.com/json-transformations/jsoncat",
+    keywords=['format', 'json', 'dump', 'console', 'terminal', 'command-line'],
+
+    author="Brian Peterson",
+    author_email="bpeterso2000@yahoo.com",
+
+    description="Concatenate JSON FILE(s), or STDIN, format to STDOUT.",
+    long_description=readme,
+
+    packages=find_packages(include=['jsonls']),
+    include_package_data=True,
+    zipsafe=False,
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+    ],
+
+    install_requires=['click==6.7', 'contextlib2===0.5.5'],
+
+    test_suite='tests',
+    test_requires=['flake8', 'mock', 'pytest-cov', 'tox'],
+
+    setup_requires=['pytest-runner'],
+
+    entry_points={'console_scripts': ['jsoncat=cli:jsoncat']}
 )
